c6e5333078a1a91202316fb92b14dac6f350130a
Nao Suzuki <yonkzs@gmail.com>
2022-05-24 10:52:37 +0200

    display events, refactor and implement remove event button

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,9 @@
 import React, { useReducer, useState } from 'react';
 
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-import reducer from '../reducers'
+import Event from './Event';
+import reducer from '../reducers';
 
 const App = () => {
   const [state, dispatch] = useReducer(reducer, []);
@@ -54,7 +55,7 @@ const App = () => {
           </tr>
         </thead>
         <tbody>
-
+          { state.map((event, index) => (<Event key={index} event={event} dispatch={dispatch} />))}
         </tbody>
       </table>
     </div>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -41,10 +41,10 @@ const events = (state = [], action) => {
       const event = { title: action.title, body: action.body };
       const length = state.length;
       const id = length === 0 ? 1 : state[length - 1].id + 1;
-      return [...state, { id, ...event }]
+      return [...state, { id, ...event }];
     case 'DELETE_EVENT':
-      return state;
-    case 'CREATE_ALL_EVENT':
+      return state.filter(event => event.id !== action.id);
+    case 'DELETE_ALL_EVENT':
       return [];
     default:
       return state;
